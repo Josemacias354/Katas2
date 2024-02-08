2c06784afa528718fa70f0610afcc6c7768801f6
Josemacias354 <maciasbichillo1@gmail.com>
2024-02-08 10:25:28 +0100

    update

--- src/main/java/org/Jose/Kata.java
+++ src/main/java/org/Jose/Kata.java
@@ -5,5 +5,6 @@ public class Kata {
         return String.format("(%d%d%d) %d%d%d-%d%d%d%d",
                 numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5],
                 numbers[6], numbers[7], numbers[8], numbers[9]);
+
     }
 }
